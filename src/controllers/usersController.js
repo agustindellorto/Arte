c9ad57ba5fc4011c780dbe197c0b6a8ff4331a96
belusangui <belu.sanguinetti@gmail.com>
2021-07-30 14:15:54 +1000

    perfil usuario controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+const usersFilePath = path.join(__dirname, '../database/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 let controller = {
     login: (req, res)=>{
@@ -9,9 +13,19 @@ let controller = {
     store: (req, res) => {
         console.log("Aca va la logica de crear usuario en el archivoJson");
     },
+
     perfilUsuario: (req, res) => {
-        res.render('perfil_usuario');
+        let idUser = req.params.id;
+        let usuarioEncontrado;
+        for (i=0; i<users.length; i++) {
+            if(users[i].id == idUser) {
+                usuarioEncontrado = users[i];
+            }
+        }
+        
+        res.render('perfil_usuario', {usuario: usuarioEncontrado});
     },
+
     edit: (req, res) => {
         console.log("Logica Editar usuario, no es necesaria para sprint 4")
     },
